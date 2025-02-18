1e7292b4a82d3bee4cdcd5c9c0488f25031ad5d9
Tssemi <127999956+tssemi@users.noreply.github.com>
2025-02-18 09:36:29 -0300

    Create two variables, one for the last button element, the other to reset all given inputs

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function addBookToLibrary() {
     let author = document.querySelector('#author');
     let numPages = document.querySelector('#pages');
     let read = document.querySelector('input[type="radio"]');
-    
+
     myLibrary.push(new Book(title.value, author.value, numPages.value, read.value))
     console.log(myLibrary);
 }
@@ -23,3 +23,13 @@ submit.addEventListener("click", (e) => {
     e.preventDefault()
     addBookToLibrary()
 });
+
+const newBook = document.querySelector('.newBook');
+const books = document.querySelectorAll('.form div input');
+
+
+newBook.addEventListener("click", (e) => {
+    books.forEach(e => {
+        e.value = '';
+    });
+})
